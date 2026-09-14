d7dd6a6abde4e15d7c50c4773b72819451894619
Carlosmartinez078 <00171125@uca.edu.sv>
2026-09-14 12:10:18 -0600

    add files

---
+++ exercise.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+struct Elemento {
+    int codigo, longitud,capacidad,utlizacion;
+    std::string nombre;
+    std::string seguridad;
+    float cargas [3];
+};
+
+void registrarElementos (Elemento* Pelemento);
+void promedio (Elemento* Pelemento);
+
+int main(){
+    Elemento elemento1;
+
+    registrarElementos(&elemento1);
+
+    return 0;
+}
+
+
+
+void registrarElementos (Elemento* Pelemento){
+    const int n = 3;
+    Pelemento->cargas[n];
+    
+    std::cout << "Ingrese el codigo del elemento" << std::endl;
+    std::cin >> Pelemento->codigo;
+    std::cout << "Ingrese el nombre del elemento" << std::endl;
+    std::cin >> Pelemento->nombre;
+    std::cout << "Ingrese la longitud del elemento" << std::endl;
+    std::cin >> Pelemento->longitud;
+    std::cout << "Ingrese la capacidad maxima" << std::endl;
+    std::cin >> Pelemento->capacidad;
+    std::cout << "Ingrese las cargas" << std::endl;
+    for (int i = 0; i < n; i++){
+        std::cin >> Pelemento->cargas[n];
+        std::cout << "Elemento: " << i+1 << std::endl;
+        for (int i = 0; i < n; i++){
+        }
+        std::cout << Pelemento->cargas[n] << std::endl;
+    }
+}
+
+void promedio (Elemento* Pelemento){
+    float promedio = Pelemento->cargas/3;
+
+    }
+     
+    
